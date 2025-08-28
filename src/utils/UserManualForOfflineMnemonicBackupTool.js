aa93db6dabbecf5eec19f752110c89aa0946545f
qiYu <1322941394@qq.com>
2025-08-28 21:19:14 +0800

    new_web3User

--- src/utils/UserManualForOfflineMnemonicBackupTool.js
+++ src/utils/UserManualForOfflineMnemonicBackupTool.js
@@ -1,4 +1,17 @@
 // src/utils/UserManualForOfflineMnemonicBackupTool.js
+import imKeyL1Pro_imKey from '@/public/images/imKeyL1Pro/imKeyL1Pro&imKey.png'
+import imKeyL1ProUse from '@/public/images/imKeyL1Pro/imKeyL1ProUse.png'
+import imKeyL1Pro from '@/public/images/imKeyL1Pro/imKeyL1Pro.png'
+import imKeyP1All from '@/public/images/imKeyP1/imKeyP1All.png'
+import imKeyP1User1 from '@/public/images/imKeyP1/imKeyP1User1.png'
+import imKeyP1User2 from '@/public/images/imKeyP1/imKeyP1User2.png'
+import imKeyP1User3 from '@/public/images/imKeyP1/imKeyP1User3.png'
+import imKeyP1UserPW from '@/public/images/imKeyP1/imKeyP1UserPW.png'
+import imKeyP1UserSC from '@/public/images/imKeyP1/imKeyP1UserSC.png'
+import imKeyS1All from '@/public/images/imKeyS1/imKeyS1All.png'
+import imKeyS1User from '@/public/images/imKeyS1/imKeyS1User.png'
+import imKeyS1User2 from '@/public/images/imKeyS1/imKeyS1User2.png'
+
 import img_1 from '@/public/images/imKeyL1Pro/img_1.png'
 
 const pageContents = [
@@ -45,9 +58,204 @@ const pageContents = [
 <p><strong>操作便捷 </strong>每个助记词只需记录前 4 个字母即可，冲压板配备冲压笔，操作简单方便，用户可以轻松、快速的完成打孔备份。</p>
 <p><strong>价格实惠 </strong>作为一种经济实惠的选择，冲压板 L1 Pro 提供了比纸质备份更高的安全性和耐用性，是多重备份的优选工具！</p>
 
-                
+<h3>操作步骤图示</h3>
+<ol>
+<li>
+使用 imKey 硬件钱包创建钱包，并用助记词卡片抄写备份助记词。 
+<span style="color: #999;"> 根据 BIP39 规范，每个单词的前 4 个字母即可确定唯一的助记词。即在冲压刻字时只需冲压每个单词的前 4 个字母即可，例如你想冲压刻字 abandon，则只需冲压刻字 aban。（对于只有 3 个字母的单词，第四位留白即可。）</span>
+<img src="${imKeyL1Pro_imKey}" alt="imKeyL1Pro_imKey">
+</li>
+<li>
+按照助记词顺序依次在冲压板上冲压刻字。1 块冲压板的正反两面均可使用，1 块冲压板可备份 12 个助记词。
+<img src="${imKeyL1ProUse}" alt="imKeyL1ProUse">
+</li>
+<li>
+冲压完成后，将冲压板存放在安全的地方，防止窥探、丟失。
+<img src="${imKeyL1Pro}" alt="imKeyL1Pro">
+<p>温馨提示： <span style="color: darkred"> 如果你对助记词备份进行了二次加密，请务必记录好你自定义的加密规则，并将其与钱包分别存放于不同的地方进行保管。</span></p>
+</li>
+</ol>
+
+<h3>常见问题解答</h3>
+<strong>每个助记词为什么只需要记录前 4个字母，没问题吗？</strong>
+<p>根据 BIP39 定义的助记词列表规则，用户只需记录每个单词的前 4 个字母即可推导出完整的助记词。例如你的助记词是： desert shift awesome，那么密盒中只需记录 dese shif awes 即可。同时，在 imKey 上使用助记词恢复钱包时输入前 4 个字母，钱包就会出现唯一单词，所以，你不用为 L1 Pro 不能备份完整助记词的担心。</p>
+<strong>打孔深度问题说明</strong>        
+<p>有用户担心冲压板打孔深度不够。实际上，冲压笔的打孔深度比传统字母板雕刻深度更深。我们通过高温燃烧等测试后，打孔点位依旧清晰可见，可以确保助记词备份安全。用户无需担心打孔深度不足的问题。另外，打孔完成后，侧着从不同角度看，助记词孔位会更明显。</p>    
+
+<h3>总结</h3>
+<p>imKey 助记词冲压板 L1 Pro 是一款安全可靠的助记词保管工具，采用 304 不锈钢制造，具备防水、防火和耐腐蚀特性，完全离线使用，有效保护你的数字资产安全。使用简便，适合于各类加密货币钱包的助记词备份和管理。</p>
+<a href="https://www.imtoken.org/" target="_blank">密盒 L1 Pro购买链接</a>
         `
             },
+            {
+                id: 'file1_2',
+                title: '什么是密盒 P1，如何使用？',
+                content: `
+                <ol><li> <a href="https://shop42404838.m.youzan.com/wscgoods/detail/36bm6keap5t1a33?dc_ps=3340498535169757189.200001" target="_blank"> 密盒 P1 </a>是一款袖珍型密钥保管工具，内含一套不锈钢钢板和一套字符块，能保管多达 128 个字符的密钥信息。可用于组装和存储助记词、私钥和绑定码等密钥信息，支持备份 1Password、Google 密码管理器等生成的密码</li></ol>
+                
+                <h3>密盒 P1 使用视频教程</h3>
+                <div class="video-container">
+    <iframe 
+        src="//player.bilibili.com/player.html?isOutside=true&aid=979924452&bvid=BV1o44y1P7hW&cid=555022365&p=1" 
+        frameborder="0" 
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+        loading="lazy"
+        referrerpolicy="no-referrer"
+        title="手把手教你使用密盒 Pro 版"
+        class="responsive-iframe">
+    </iframe>
+</div>
+
+<h3>基本信息</h3>
+<img src="${imKeyP1All}" alt="imKeyP1All">
+<ul>
+<li>密盒尺寸: 86mm x 86mm x 6mm</li>
+<li>密盒重量: 260g</li>
+<li>包装材质: 纸盒</li>
+<li>密盒组成: 304 不锈钢制成盒体和字符块</li>
+<li>字符数量: 520 颗字符块（含英文大小写字母、数字（0-9）和 ASCII 字符，合计 1040 个字符）</li>
+</ul>
+
+<h3>组装教程</h3>
+<ol>
+<li>旋开前面板
+<p>逆时针旋开盒体的前面板，直到可以看到完整的 A 面。</p>
+</li>
+<li>拧开螺丝
+<p>使用螺丝刀松开所有的螺丝。</p>
+</li>
+<li>旋开盖板
+<p>逆时针旋开盖板。</p>
+</li>
+<li>填入字符
+<p>字符块套件中有 520 个字符块，正反两面均刻有字符，因此共有 1040 个字符。</p>
+<p>从字符块套件中找到你要使用的字符，并用螺丝刀戳下填入盒体的格子中。</p>
+<p>若你保管的是符合 BIP39 协议的助记词，在填入字符时只需填入每个单词的前 4 个字母即可，例如你想填入单词 abandon，则只需填入 aban。（对于只有 3 个字母的单词，可通过空白字符块来填充第四个字符位。）</p>
+<p>盒体的 A/B 两面均可使用，能保管多达 128 个字符的密钥信息，除了可用于保管助记词，还可用于保管私钥、绑定码（如有）等各类信息。</p>
+</li>
+<li>关闭密盒
+<p>回旋盖板并拧紧螺丝，确认密钥信息备份无误后再回旋前面板。</p>
+</li>
+<li>贴上不可逆封条贴纸
+<p>贴上不可逆封条贴纸并存放在安全的地方，防止窥探。</p>
+<img src="${imKeyP1User1}" alt="imKeyP1User1">
+<img src="${imKeyP1User2}" alt="imKeyP1User2">
+<img src="${imKeyP1User3}" alt="imKeyP1User3">
+</li>
+</ol>
+
+<h3>密码保管最佳实践</h3>
+<p>密盒 P1 盒体的 A/B 两面均可使用，能保管多达 128 个字符的密钥信息，可以保管 2 套助记词/私钥。</p>
+<p>密盒 P1 的字符块集包含字母块（大小写 A-Z）、数字块（0-9）和 ASCII 字符块，除了可用于保管助记词，还可用于保管 PIN 码、私钥、绑定码和其他密码等各类信息。</p>
+<img src="${imKeyP1UserPW}" alt="imKeyP1UserPW">
+<span style="color: #999">注：地址可保存最后 8 位用于不同钱包的区分。</span>
+
+<h3>自定义加密</h3>
+<p>密盒 P1 A/B 两面的字符格都拥有单独的编号，支持你对所保存的密钥进行自定义加密，从而更好地防窥和防盗。</p>
+<p>例如在盒体内填入正确的助记词后，可将 E3 与 F6，E4 与 G6，E5 与 H6 字符格的字符块互换实现加密，这样即便他人获取了你的密盒但由于不知道你的加密规则，仍无法知道你的助记词。</p>
+<img src="${imKeyP1UserSC}" alt="imKeyP1UserSC">
+<strong>温馨提示：</strong>
+<p><span style="color: darkred">请务必记录好你自定义的加密规则，并将其与密盒分开存放于不同的地方进行保管。</span></p>
+
+<h3>常见问题解答</h3>
+<strong>为什么仅推荐高阶用户购买？</strong>
+<p>密盒 P1 是一款多功能的密钥保管工具，内含一套不锈钢钢板和一套字符块，能保管多达 128 个字符的密钥信息，可用于组装和存储助记词、私钥和绑定码等密钥信息，支持备份 1Password、Google 密码管理器等生成的密码，支持信息的二次加密，极大的提升了安全等级，有效防窥。</p>
+<p>相比密盒 S1，密盒 P1 更复杂，具有更多的可组合性，如有采用二次加密，请务必备份好二次加密规则。</p>
+<p><strong>每个助记词为什么只需要记录前 4个字母，没问题吗？</strong></p>
+<p>根据 BIP39 定义的助记词列表规则，用户只需记录每个单词的前 4 个字母即可推导出完整的助记词。例如你的助记词是： desert shift awesome，那么密盒中只需记录 dese shif awes 即可。同时，在 imKey 上使用助记词恢复钱包时输入前 4 个字母，钱包就会出现唯一单词，所以，你不用为密盒 P1 不能备份完整助记词的担心。</p>
+
+<h3>总结</h3>
+<p>如果你是资深玩家，对助记词、私钥和密码等重要内容有更安全的保管需求，我们推荐使用
+ <a href="https://shop42404838.m.youzan.com/wscshop/showcase/feature?alias=2xL8D9kBdt&banner_id=f.77421469~image_ad.2~0~zSCTleYM&reft=1721718357142&spm=g.711638544_f.77421469" target="_blank">密盒</a> P1 进行自定义加密离线备份，确保资产和账号安全。</p>
+<p>密盒 P1 购买链接：</p>
+<a href="https://shop42404838.m.youzan.com/wscgoods/detail/36bm6keap5t1a33?scan=1&activity=none&from=kdt&qr=directgoods_1615698499&shopAutoEnter=1" target="_blank">https://j.youzan.com/IXAUfF</a>
+                
+                
+                `
+            },
+            {
+                id: 'file1_3',
+                title: '什么是密盒 S1，如何使用？',
+                content: `
+                <p>
+                <a href="https://shop42404838.m.youzan.com/wscgoods/detail/2xj16xca3nkmm?banner_id=f.88725471~goods.10~1~RIqvipvp&alg_id=common_by_external%2Cnot_need_rank%2C0.0.0.0.0.0.0.0.0.0_c5f020898ee34f1a88f0de441934a379&slg=0&components_style_layout=2&reft=1696753167981_1696753175605&spm=f.77421469_f.88725471" target="_blank">密盒 S1 </a>
+                是一款高性价比的助记词保管工具，通体由 304 级不锈钢打造，AB 两面可以存放 2 套 12 位的助记词。与传统的纸质抄写相比，密盒 S1 具有防水、防火、防腐蚀的特性。
+</p>
+<p>视频教程：</p>
+                <div class="video-container">
+    <iframe 
+        src="//player.bilibili.com/player.html?isOutside=true&aid=280330659&bvid=BV1sc411d7eh&cid=1315638022&p=1" 
+        frameborder="0" 
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+        loading="lazy"
+        referrerpolicy="no-referrer"
+        title="imKey 密盒 S1 开箱视频"
+        class="responsive-iframe">
+    </iframe>
+</div>
+
+<h3>密盒 S1 里有什么</h3>
+<p>密盒 S1 包含：</p>
+<ul>
+<li>1 x 盒体</li>
+<li>3 x 字母板（252 个字符块，504 个字符）</li>
+<li>1 x 说明书</li>
+<li>1 x 螺丝刀</li>
+<li>3 x 备用螺丝</li>
+</ul>
+<img src="${imKeyS1All}" alt="imKeyS1All">
+<p>确认包装盒内的配件完整后即可使用。</p>
+
+<h3>如何使用密盒 S1</h3>
+<ol>
+<li>打开前面板
+<p>逆时针旋开盒体的前面板</p>
+</li>
+<li>拧开螺丝
+<p>使用螺丝刀拧开所有螺丝</p>
+</li>
+<li>旋开盖板
+<p>逆时针旋开盖板。</p>
+</li>
+<li>填入字符
+<p>从字母板中取出你要使用的字母块，填入盒体的格子中，每个空槽可以放置 4 个字母块。</p>
+<p><span style=" color: #999">注：根据 BIP39 规范，每个单词的前 4 个字母即可确定唯一的助记词。例如你想填入单词 abandon，则只需填入 aban。如果遇到 3 个字母组成的助记词，可通过空白字母块来填充第四个字母位。</span></p>
+</li>
+<li>关闭密盒
+<p>回旋盖板并拧紧螺丝，确认助记词备份无误后再回旋前面板。</p>
+</li>
+</ol>
+<img src="${imKeyS1User}" alt="imKeyS1User">
+<p>温馨提醒：</p>
+<p>请将密盒 S1 存放在安全的地方，必要时可加锁防止窥探。</p>
+
+<h3>常见问题解答</h3>
+<strong>我需要密盒 S1 吗？</strong>
+<p>
+根据行业知名安全公司慢雾的《
+ <a href="https://web3caff.com/archives/98153"> 2024 Q2 MistTrack 被盗表单分析</a>报告总结，私钥泄露是 2024 年第二季度被盗事件的主要原因。导致私钥泄露的关键因素是用户未离线备份助记词或下载了假钱包。为此，我们强烈推荐密盒 S1。
+</p>
+<p>
+密盒 S1 通体由 304 不锈钢精密制造，相比纸张备份助记词，是更安全的离线保管助记词通用方案，不仅可以保护数字资产免受网络黑客的攻击，同时具有有效抵抗各种物理损坏的能力，包括火灾、浸水、腐蚀以及坠落冲击等。
+</p>
+<p>
+相比同类型产品，密盒 S1 在诸多细节上做了安全增强，比如字母块厚度达到 1mm，盖板和盒体采用 5 颗螺丝锁紧等，为了资产更安全，你需要密盒 S1。
+</p>
+<strong>每个助记词为什么只需要记录前 4个字母，没问题吗？</strong>
+<p>
+根据 BIP39 定义的助记词列表规则，用户只需记录每个单词的前 4 个字母即可推导出完整的助记词。例如你的助记词是： castle transfer polesph，那么密盒中只需记录 cast tran pole 即可。同时，在 imKey 上使用助记词恢复钱包时输入前 4 个字母，钱包就会出现唯一单词，所以，你不用为密盒 S1 不能备份完整助记词的担心。
+</p>
+<img src="${imKeyS1User2}" alt="imKeyS1User2">    
+<p>
+为了你的资产安全，我们推荐使用
+ <a href="https://shop42404838.m.youzan.com/wscshop/showcase/feature?alias=2xL8D9kBdt&banner_id=f.77421469~image_ad.2~0~zSCTleYM&reft=1721718357142&spm=g.711638544_f.77421469"> 不锈钢密盒</a>进行离线备份助记词，其防水、防火、防腐蚀特性为助记词提供可靠保护。
+</p>    
+<p>密盒 S1 购买链接：</p>
+<a href="https://shop42404838.m.youzan.com/wscgoods/detail/2xj16xca3nkmm?scan=1&activity=none&from=kdt&qr=directgoods_711638544&shopAutoEnter=1">https://j.youzan.com/USDlfF</a>
+                
+                
+                `
+            }
         ]
     }
 ];
